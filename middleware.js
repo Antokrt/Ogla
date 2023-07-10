c26df8d9e09aa6a754cb63a237b70905631f969a
Tao <a.koreta@outlook.fr>
2023-07-10 15:31:59 +0200

    commit new Middleware

--- middleware.js
+++ middleware.js
@@ -1,40 +1,40 @@
 import { NextResponse} from "next/server";
-import {getToken} from "next-auth/jwt";
 import {GetCategory} from "./pages/api/utils/Category";
+import {withAuth} from "next-auth/middleware";
 
-export async function middleware(req){
+export default withAuth(
+    function middleware(req){
 
-    const token = await getToken({
-        req,
-        secret:process.env.NEXT_AUTH_SECRET
-    });
+        const token = req.nextauth.token;
 
-    if (req.nextUrl.pathname.startsWith('/dashboard') && !token) {
-        return NextResponse.redirect(new URL('/', req.url))
+        if (req.nextUrl.pathname.startsWith('/dashboard') && !token) {
+            return NextResponse.redirect(new URL('/', req.url))
+        }
+
+        if (req.nextUrl.pathname.startsWith('/profil') && !token) {
+            return NextResponse.redirect(new URL('/', req.url))
+        }
+
+
+        if (req.nextUrl.pathname.startsWith('/bibliotheque/') && !GetCategory().includes(req.nextUrl.pathname.split('/bibliotheque/')[1])) {
+            return NextResponse.redirect(new URL('/bibliotheque/', req.url));
+        }
+
+
+        if (req.nextUrl.pathname.startsWith('/dashboard') && token && !token?.is_author) {
+            return NextResponse.redirect(new URL('/', req.url))
+        }
+
+        if(req.nextUrl.pathname === '/dashboard'){
+            return NextResponse.redirect(new URL('/dashboard/books',req.url))
+        }
+
+
+
+        if(req.nextUrl.pathname === '/devenir-ecrivain' && token && token?.is_author){
+            return NextResponse.redirect(new URL('/bibliotheque',req.url))
+        }
     }
+)
 
-    if (req.nextUrl.pathname.startsWith('/profil') && !token) {
-        return NextResponse.redirect(new URL('/', req.url))
-    }
-
-
-    if (req.nextUrl.pathname.startsWith('/bibliotheque/') && !GetCategory().includes(req.nextUrl.pathname.split('/bibliotheque/')[1])) {
-        return NextResponse.redirect(new URL('/bibliotheque/', req.url));
-    }
-
-
-    if (req.nextUrl.pathname.startsWith('/dashboard') && token && !token.is_author) {
-        return NextResponse.redirect(new URL('/', req.url))
-    }
-
-    if(req.nextUrl.pathname === '/dashboard'){
-        return NextResponse.redirect(new URL('/dashboard/books',req.url))
-    }
-
-
-
-    if(req.nextUrl.pathname === '/devenir-ecrivain' && token && token.is_author){
-        return NextResponse.redirect(new URL('/bibliotheque',req.url))
-    }
-}
 
