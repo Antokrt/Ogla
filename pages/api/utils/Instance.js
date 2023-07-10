959719adac1939ace458c6e063f532c13897d571
Tao <a.koreta@outlook.fr>
2023-07-10 14:43:54 +0200

    commit fix path fetch2

--- pages/api/utils/Instance.js
+++ pages/api/utils/Instance.js
@@ -1,7 +1,7 @@
 export const GetFetchPath = () => {
     if (process.env.NODE_ENV === "development") {
         return 'http://localhost:3008/';
-    } else (process.env.NODE_ENV === "production") {
+    } else {
         return 'https://ogla-api-4fca21bb1e56.herokuapp.com/';
     }
 }
