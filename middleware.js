3d51a2b714c0bf5993aa4578bc0eef9109342743
Tao <a.koreta@outlook.fr>
2023-07-11 11:36:58 +0200

    add middleware

--- middleware.js
+++ middleware.js
@@ -47,7 +47,34 @@ export const config = {
 }*/
 
 
+/*
 export { default} from 'next-auth/middleware';
+
+export const config = {
+    matcher: ['/dashboard/:path*', '/profil/:path*', ]
+}
+*/
+
+
+import { withAuth} from "next-auth/middleware";
+import {NextResponse} from "next/server";
+
+export default withAuth(
+    function middleware(req){
+        const token = req.nextauth.token;
+
+
+        if(req.nextUrl.pathname === '/dashboard'){
+            return NextResponse.redirect(new URL('/dashboard/books',req.url))
+        }
+
+    },
+    {
+        callbacks: {
+            authorized: ({ token }) => token,
+        },
+    }
+)
 
 export const config = {
     matcher: ['/dashboard/:path*', '/profil/:path*', ]
