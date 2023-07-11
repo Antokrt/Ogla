0835e961274d5ed6de2983d249b66797311410a0
Tao <a.koreta@outlook.fr>
2023-07-11 15:41:55 +0200

    add sett

--- pages/profil/index.js
+++ pages/profil/index.js
@@ -123,7 +123,7 @@ const Profil = ({profilData, err}) => {
 
     const updateSettingsOfSession = () => {
         return new Promise((resolve, reject) => {
-            instance.get(GetApiPath() + 'api/auth/session?new-settings')
+            axios.get(GetApiPath() + 'api/auth/session?new-settings')
                 .then(() => resolve())
                 .catch((err) => {
                     console.log('err settings')
