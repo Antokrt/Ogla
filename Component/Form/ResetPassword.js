30e22fa2db184686c6d49fe576bdb0e47fd52ab2
alexis gachet <gachet.alexisfc@gmail.com>
2023-07-12 20:26:11 +0200

    debut d'ajout de variable  et

--- Component/Form/ResetPassword.js
+++ Component/Form/ResetPassword.js
@@ -1,21 +1,15 @@
 import styles from "../../styles/Pages/Form/ResetPassword.module.scss";
-import { ArrowDownIcon } from "@heroicons/react/24/outline";
-import Category from "../../json/category.json";
-import { Capitalize } from "../../utils/String";
-
-import scrollbar from "../../styles/utils/scrollbar.module.scss";
-import { useSession, signIn, signOut } from "next-auth/react";
-import React, { useEffect, useRef, useState } from "react";
-import { router, useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import React, { useRef, useState } from "react";
+import { useRouter } from "next/router";
 import { SendNewPasswordWhenForgot } from "../../service/User/Password.service";
 import { instance } from "../../service/config/Interceptor";
 import { ReloadSession } from "../../utils/ReloadSession";
 import ScreenSize from "../../utils/Size";
-import {GetImgPathOfAssets} from "../../utils/ImageUtils";
+import { GetImgPathOfAssets, GetLogoUtils } from "../../utils/ImageUtils";
 
 const ResetPasswordForm = ({ email, token, id }) => {
 
-    const { data: session, status } = useSession();
     const formRef = useRef(null);
     const [newPassword, setNewPassword] = useState('');
     const [confPassword, setConfPassword] = useState('');
@@ -56,14 +50,16 @@ const ResetPasswordForm = ({ email, token, id }) => {
         <div className={styles.formContainer}>
             <div className={styles.leftBlock}>
                 <div className={styles.container}>
-                    <div className={styles.LogoHead}>
-                        <h1 onClick={() => router.push("/")}> OGLA </h1>
+                    <div className={styles.imgAbs} onClick={() => router.push("/")}>
+                        <img
+                            src={GetLogoUtils()}
+                            alt={'Logo Ogla'}
+                            onError={(e) => e.target.src = '/assets/logo/mountain.png'}
+                        />
                     </div>
-
 
                     <div className={styles.headerTwo}>
                         <h1>Modifier votre mot de passe</h1>
-
                     </div>
 
                     <form onSubmit={handleSubmit} ref={formRef} className={styles.form}>
@@ -73,13 +69,11 @@ const ResetPasswordForm = ({ email, token, id }) => {
                             <input type={"password"} onChange={(e) => setNewPassword(e.target.value)} name={"password"} placeholder={"Nouveau mot de passe"}></input>
                             <label htmlFor={"password"}>Confirmer le mot de passe</label>
                             <input type={"password"} onChange={(e) => setConfPassword(e.target.value)} name={"password"} placeholder={"Confirmer le mot de passe"} />
-
                         </div>
                         {
                             submitErr.show &&
                             <p className={styles.submitErr + ' ' + styles.fadeIn}>{submitErr.msg}</p>
                         }
-
                         <div className={styles.stepBtnContainer}>
                             <button type={'submit'} className={confPassword === newPassword ? styles.newPassBtn : styles.disabledBtn + ' ' + styles.newPassBtn}>Modifier</button>
                         </div>
@@ -91,8 +85,8 @@ const ResetPasswordForm = ({ email, token, id }) => {
                 width > 1000 &&
                 <div className={styles.containerImg}>
                     <img alt={'Image Bannière Modifier Mot de passe Ogla'}
-                         onError={(e) => e.target.src = '/assets/diapo/knight.png'}
-                         src={GetImgPathOfAssets() + 'diapo/knight.png'} />
+                        onError={(e) => e.target.src = '/assets/diapo/knight.png'}
+                        src={GetImgPathOfAssets() + 'diapo/knight.png'} />
                 </div>
             }
         </div>
