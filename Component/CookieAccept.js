9db8619ff97a1118837f2803c208fc12f6d5ec6e
Tao <a.koreta@outlook.fr>
2023-07-12 18:48:38 +0200

    add cookie

--- Component/CookieAccept.js
+++ Component/CookieAccept.js
@@ -35,22 +35,43 @@ export const CookieAccept = () => {
         <CookieConsent
             onAccept={() => setCookieConsent(true)}
             onDecline={() => setCookieConsent(false)}
+            buttonWrapperClasses={'btn-consent-container'}
             enableDeclineButton
-            flipButtons
+            declineButtonText={'Je refuse'}
             location="bottom"
-            buttonText="I understand"
-            cookieName="YourCoockieName"
-            style={{ background: 'blue' }}
+            buttonText="J'accepte"
+            cookieName="cons_cookie"
+            style={{
+                display:'flex',
+                justifyContent:'align-center',
+                alignItems:'align-center',
+                background: '#9844AD',
+                borderTopLeftRadius: "10px",
+                borderTopRightRadius: "10px",
+                fontFamily: 'Poppins',
+                fontSize: '12px'
+            }}
             buttonStyle={{
-                color: '#000',
-                fontSize: '15px',
+                color: '#9844AD',
+                fontFamily: 'Poppins',
+                background: 'white',
+                borderRadius: '4px',
+                fontSize: '12px',
             }}
             declineButtonStyle={{
-                margin: '10px 10px 10px 0',
+                color: '#9844AD',
+                borderRadius: '4px',
+                fontFamily: 'Poppins',
+                background: 'white',
+                fontSize: '12px',
             }}
             expires={450}
         >
-            This website uses cookies
+
+            Nous souhaitons améliorer votre expérience sur Ogla et rendre votre visite encore plus agréable. <br/> En
+            acceptant l'utilisation des cookies, vous nous permettez d'analyser les données et de vous offrir un service
+            personnalisé.
         </CookieConsent>
+
     );
 }
