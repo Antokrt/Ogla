745d3092ac39c2aba692cd3776327e092b089f0f
Tao <a.koreta@outlook.fr>
2023-12-23 09:06:01 +0100

    new img

--- Component/Maintenance.js
+++ Component/Maintenance.js
@@ -1,9 +1,10 @@
 import styles from '../styles/Component/Maintenance.module.scss';
+import Image from "next/image";
 
 export const Maintenance = () => {
     return (
         <div className={styles.container}>
-            <img src={'/assets/diapo/old.png'}/>
+            <Image width={300} height={300}  src={'/assets/diapo/old.png'}/>
             <p>Ogla est en cours de maintenance. <br/> Revenez bientôt !</p>
         </div>
     )
